138f752fe26ddeeed9f848e901ef266387fd88f3
Robin Debel <robindebel@hotmail.nl>
2020-04-21 15:50:53 +0100

    made connection between database and html

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,6 +17,22 @@ def login():
 @app.route("/profile")
 def profile():
 	return render_template("profile.html")
+
+@app.route("/categories")
+def categories():
+	return render_template("categories.html", categories=getcategories())
+
+def getcategories():
+	conn = sqlite3.connect("database/UberNeeds.db")
+	cursor = conn.execute("select * from Categories")
+	data = []
+
+	for row in cursor:
+		data.append({"name":row[1],
+					"description": row[2]})
+	conn.close()
+
+	return data
 
 
 if __name__ == "__main__":
