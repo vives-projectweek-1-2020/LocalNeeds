82195382d73664ef27be84768d0d938f7d41a57f
Robin Debel <robindebel@hotmail.nl>
2020-04-21 15:06:30 +0100

    added profile to app.py

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -2,8 +2,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def homepage():
+def index():
 	return render_template("index.html", myvariable="Hi")
+
+@app.route("/homepage")
+def homepage():
+	return render_template("homepage.html")
+
+@app.route("/login")
+def login():
+	return render_template("login.html")
+
+@app.route("/profile")
+def profile():
+	return render_template("profile.html")
 
 
 if __name__ == "__main__":
